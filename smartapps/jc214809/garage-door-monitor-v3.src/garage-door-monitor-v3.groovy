25e39fa0c2638250dd0c3ef29cd70502e1f560d1
Joel Jayson Clark <jc214809@ohio.edu>
2018-03-22 12:37:20 -0400

    changed time

--- smartapps/jc214809/garage-door-monitor-v3.src/garage-door-monitor-v3.groovy
+++ smartapps/jc214809/garage-door-monitor-v3.src/garage-door-monitor-v3.groovy
@@ -179,13 +179,13 @@ def checkDoors() {
     doors?.each{ door ->
         def doorName = door.displayName
         def doorOpen = checkDoor(door)
-        def readableNowTime = new Date(now()).format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'));
+        def readableNowTime = new Date(now()).format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('EST'));
 
         if (doorOpen == "open") {
             log.debug("checkDoors: Should times be set? ${state.timeToClose == null}")
             if (state.timeToClose == null) {
                 state.timeToClose = new Date(now() + 9*60*1000);
-                def readableCloseTime = state.timeToClose.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('UTC'))
+                def readableCloseTime = state.timeToClose.format("yyyyMMdd-HH:mm:ss.SSS", TimeZone.getTimeZone('EST'))
                 
                 log.debug("checkDoors: When to close ${state.timeToClose}")
                 log.debug("checkDoors: readableCloseTime: ${readableCloseTime}")
